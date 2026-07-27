49bed97a1ba0e304620fe6bdc0583cf29be45e03
Atharv Patil <atharvdpatil909@gmail.com>
2026-07-27 21:55:15 +0530

    Empty states done....:)):- empty_search, empty_favourite, empty_board

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,9 +1,38 @@
-const DashboardPage = () => {
-    return (
-        <div>
-            Dashboard Root page
-        </div>
-    );
+"use client";
+
+import { use } from "react";
+import { useOrganization } from "@clerk/nextjs";
+
+import { EmptyOrg } from "./_components/empty-org";
+import { BoardList } from "./_components/board-list";
+
+interface DashboardPageProps {
+  searchParams: Promise<{
+    search?: string;
+    favorites?: string;
+  }>;
+}
+
+const DashboardPage = ({
+  searchParams
+}: DashboardPageProps) => {
+  const { organization } = useOrganization();
+  
+  // Unwrap the searchParams promise using React.use()
+  const query = use(searchParams);
+
+  return ( 
+    <div className="flex-1 h-[calc(100%-80px)] p-6">
+      {!organization ? (
+        <EmptyOrg />
+      ) : (
+        <BoardList
+          orgId={organization.id}
+          query={query}
+        />
+      )}
+    </div>
+  );
 };
 
 export default DashboardPage;
