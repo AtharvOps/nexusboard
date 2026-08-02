1fc8f32fc78c95abdd92ad0b46f719d9cda94862
Atharv Patil <atharvdpatil909@gmail.com>
2026-08-03 04:38:35 +0530

    strip adding basic work...

--- convex/schema.ts
+++ convex/schema.ts
@@ -22,5 +22,16 @@ export default defineSchema({
     .index("by_board", ["boardId"])
     .index("by_user_org", ["userId", "orgId"])
     .index("by_user_board", ["userId", "boardId"])
-    .index("by_user_board_org", ["userId", "boardId", "orgId"])  
+    .index("by_user_board_org", ["userId", "boardId", "orgId"]),
+    
+  //Subscription
+    // orgSubscription: defineTable({
+    //     orgId:v.string(),
+    //     stripePriceId: v.string(),
+    //     stripeCustomerId: v.string(),
+    //     stripeSubscriptionId: v.string(),
+    //     stripeCurrentPeriodEnd: v.number(),
+    // })
+    //     .index("by_org",["orgId"])
+    //     .index("by_subscription",["stripeSubscriptionId"])  
 });
